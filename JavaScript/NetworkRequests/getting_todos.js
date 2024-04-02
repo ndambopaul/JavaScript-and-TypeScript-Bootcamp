be9f93bf7a5af7b9eb4c2eefba93d6b9ed35eeb8
Paul Ndambo <paulndambogithub@gmail.com>
2024-04-02 12:46:57 +0300

    network requests folder updated

--- JavaScript/NetworkRequests/getting_todos.js
+++ JavaScript/NetworkRequests/getting_todos.js
@@ -23,7 +23,65 @@ const getTodos = async () => {
 
 getTodos();
 
+// Getting a single item
+const getTodo = async(todoId) => {
+    let response = await fetch(`${BASE_URL}/todos/${todoId}/`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    });
 
+
+    let todo = await response.json();
+    console.log(todo);
+};
+
+
+// Creating an item
+const createTodo = async(todo) => {
+    let response = await fetch(`${BASE_URL}/todos/`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    });
+
+    let data = await response.json()
+    console.log(data)
+}
+
+const newTodoItem = {
+    'userId': 1,
+    'title': 'Learning JavaScript',
+    'completed': false
+}
+createTodo(newTodoItemodoItem);
+
+//Updating an item
+const updateTodo = async(todoId, todoItem) => {
+    let response = await fetch(`${BASE_URL}/todos/${todoId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        body: JSON.stringify(todoItem)
+    });
+
+    let data = await response.json();
+    console.log(data);
+}
+
+const todoItem = {
+    'userId': 1,
+    'title': 'Learning JavaScript and TypeScript',
+    'completed': true
+}
+updateTodo(50, todoItem);
+
+
+//Deleting an item
 const deleteTodo = async(todoId) => {
     let response = await fetch(`${BASE_URL}/todos/${todoId}`, {
         method: 'DELETE',
